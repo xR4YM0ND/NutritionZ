0a2d3091758fa123de1744e31f63e0257aa2aac6
Globox1997 <mitschke11@web.de>
2024-01-02 17:29:05 +0100

    added item mixin

---
+++ src/main/java/net/nutritionz/mixin/client/ItemStackMixin.java
@@ -0,0 +1,31 @@
+package net.nutritionz.mixin.client;
+
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.nutritionz.util.NutritionUtil;
+
+@Environment(EnvType.CLIENT)
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+
+    @Inject(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;appendTooltip(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Ljava/util/List;Lnet/minecraft/client/item/TooltipContext;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void getTooltipMixin(@Nullable PlayerEntity player, TooltipContext context, CallbackInfoReturnable<List<Text>> info, List<Text> list) {
+        if (player != null) {
+            NutritionUtil.addNutritionToolTip((ItemStack) (Object) this, list);
+        }
+    }
+
+}
