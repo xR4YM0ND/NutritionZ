976f6201da83c78ed6e5c40b32d84622b84f65a2
Globox1997 <mitschke11@web.de>
2024-01-02 16:51:03 +0100

    added dehydration compat

--- src/main/java/net/nutritionz/init/EventInit.java
+++ src/main/java/net/nutritionz/init/EventInit.java
@@ -1,5 +1,7 @@
 package net.nutritionz.init;
 
+import net.dehydration.api.DehydrationAPI;
+import net.dehydration.api.DrinkEvent;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
@@ -7,7 +9,12 @@ import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.nutritionz.NutritionMain;
+import net.nutritionz.access.HungerManagerAccess;
 import net.nutritionz.network.NutritionServerPacket;
 
 public class EventInit {
@@ -20,6 +27,21 @@ public class EventInit {
         // ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("fleshz_compat"), FabricLoader.getInstance().getModContainer("adventurez").orElseThrow(),
         // ResourcePackActivationType.DEFAULT_ENABLED);
         // }
+        if (FabricLoader.getInstance().isModLoaded("dehydration")) {
+            DrinkEvent.EVENT.register((ItemStack stack, PlayerEntity player) -> {
+                if (!player.getWorld().isClient()) {
+                    Item item = stack.getItem();
+                    if (NutritionMain.NUTRITION_ITEM_MAP.containsKey(item)) {
+                        for (int i = 0; i < NutritionMain.NUTRITION_ITEM_MAP.get(item).size(); i++) {
+                            if (NutritionMain.NUTRITION_ITEM_MAP.get(item).get(i) > 0) {
+                                ((HungerManagerAccess) player.getHungerManager()).addNutritionLevel(i, NutritionMain.NUTRITION_ITEM_MAP.get(item).get(i));
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
     }
 
 }
